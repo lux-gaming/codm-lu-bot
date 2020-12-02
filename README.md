dc82400d7789b15d99157b32d19a5a936495cafc
Nico Bachner <yo@nicob.dev>
2020-12-02 14:09:42 +0100

    improved functionality

--- README.md
+++ README.md
@@ -5,10 +5,12 @@ Custom Discord Bot for experimentation, learning and fun.
 ## Hosting the bot
 The bot is currently not doing anything because it is not actively hosted.
 
-If you wish to run the bot, simply clone this github repository and add your bot auth token in `auth_token.json`:
-``` json
-// auth_token.json
-{
-  "auth_token": "PASTE_TOKEN_HERE"
-}
-```
+If you wish to run the bot, follow these steps:
+1. Clone this github repository
+2. Install npm dependencies (`npm i`)
+3. Add your bot auth token in `.env`:
+    ``` js
+    // .env
+    AUTH_TOKEN=gnd87hmp45hs5qyss4p2p87s2mppgsp2gs4p2a7gfmp 
+    // obviously, this is not a valid token - replace it with your own
+    ```
--- bot.js
+++ bot.js
@@ -3,12 +3,12 @@ const { roles, channels } = require('./config.json');
 // create a new Discord bot session
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const { auth_token } = require('./auth_token.json');
-bot.login(auth_token);
+require('dotenv').config();
+bot.login(process.env.AUTH_TOKEN);
 
 // this event will only trigger once: on bot init
 bot.on('ready', () => {
-	console.log("--- Bot online ---");
+	console.log('\x1b[32m%s\x1b[0m', "--- Bot online ---");
 	const args = process.argv.slice(2);
 	if (args[0] == "announce") {
 		bot.channels.cache.get(channels.bots).send("Hello there! I'm back online");
@@ -23,76 +23,96 @@ bot.on('guildMemberAdd', member => {
 
 // interactive functionality
 bot.on('message', message => {
-	// for tesing only: console.log(message.author.username, ":", message.content)
+	// for testing only: console.log(message.author.username, ":", message.content)
 	
-	const command = message.content.toLowerCase().trim()
-	const args = command.split(' ');
+	const args = message.content.toLowerCase().trim().split(' ');
+	const command = args.shift();
+	console.log(command, args);
 
-	// for fun
-	if (command == 'happy') {
-		message.react('😄');
-	}
-	if (command == 'sad') {
-		message.react('😢');
-	}
-
-	// counting
-	if (args[0] == 'count') {
-		if (isNaN(args[1])) {
-			message.channel.send("How far would you like me to count?"); 
-		}
-		else {
-			for ( var i = 1 ; i <= args[1] ; i++ ) {
-				message.channel.send(i);
+	switch (command) {
+		case 'happy':
+			message.react('😄');
+			break;
+		case 'sad':
+			message.react('😢');
+			break;
+		case 'count':
+			switch (args[0]) {
+				case 'from':
+					if (args[1] != undefined) {
+						if (args[1] <= 10 ) {
+							message.channel.send("I'm counting ...");
+							for ( var i = args[1] ; i > 0 ; i-- ) {
+								message.channel.send(i);
+							}
+							message.channel.send("Done");
+						}
+						else {
+							message.channel.send("Please refrain from spamming");
+						}
+					} else {
+						message.channel.send("From what number would you like me to count down?");
+					}
+					break;
+				case 'to':
+					if (args[1] != undefined) {
+						if (args[1] <= 10 ) {
+							message.channel.send("I'm counting ...");
+							for ( var i = 1 ; i <= args[1] ; i++ ) {
+								message.channel.send(i);
+							}
+							message.channel.send("Done");
+						}
+						else {
+							message.channel.send("Please refrain from spamming");
+						}
+					}
+					else {
+						message.channel.send("Until what number would you like me to count?");
+					}
+					break;
+				default:
+					message.channel.send("How would you like me to count?"); 
 			}
-		}
-	}
-
-	// Reply with server welcome
-	if (command == 'server welcome') {
-		const welcome = new Discord.MessageEmbed()
-			.setTitle(`Hello and welcome to the **${message.guild.name}**`)
-			.setDescription("We're a community of devs")
-			.addFields(
-				{ name: 'Server Invite Link', value: '[https://discord.gg/https://discord.gg/DKKkJYj8hu](https://discord.gg/https://discord.gg/DKKkJYj8hu)' },
-				{ name: 'Rules', value: '**Read the [official discord rules and guidelines](https://discordapp.com/guidelines)**'},
-				{ name: 'Contributing', value: '**Contribute to our [homegrown bot](https://github.com/nico-bachner/dev-bot)**'}
-			)
-			.setTimestamp()
-		message.channel.send(welcome);
-	}
-
-	// simple info commands
-	if (command == 'server info') {
-		message.channel.send(new Discord.MessageEmbed()
-			.setTitle(`**${message.guild.name}**`)
-			.addFields(
-				{ name: 'Founded', value: message.guild.createdAt },
-				{ name: 'Region', value: message.guild.region },
-				{ name: 'Owner', value: message.guild.owner },
-				{ name: 'Members', value: message.guild.memberCount },
-				{ name: 'Rules', value: message.guild.rulesChannel }
-			)
-			.setTimestamp()
-		);
-	} 
-	else if (command == 'user info') {
-		return message.reply(`your username is ${message.author.username} and your user ID is ${message.author.id}`);
-	} 
-
-	// easily delete multiple messages
-	if (args[0] == 'prune') {
-		if (message.member.roles.cache.has(roles.moderator)) {
-			message.channel.bulkDelete(parseInt(args[1]) + 1);
-		} 
-		else {
-			return message.reply(`(${message.author.username}), only moderators are permitted to use this command`)
-		}
+			break;
+		case 'welcome':
+			const welcome = new Discord.MessageEmbed()
+				.setTitle(`Hello and welcome to the **${message.guild.name}**`)
+				.setDescription("We're a community of devs")
+				.addFields(
+					{ name: 'Server Invite Link', value: '[https://discord.gg/https://discord.gg/DKKkJYj8hu](https://discord.gg/https://discord.gg/DKKkJYj8hu)' },
+					{ name: 'Rules', value: '**Read the [official discord rules and guidelines](https://discordapp.com/guidelines)**'},
+					{ name: 'Contributing', value: '**Contribute to our [homegrown bot](https://github.com/nico-bachner/dev-bot)**'}
+				)
+				.setTimestamp()
+			message.channel.send(welcome);
+			break;
+		case 'info':
+			switch(args[0]) {
+				case 'server':
+					message.channel.send(
+						new Discord.MessageEmbed()
+						.setTitle(`**${message.guild.name}**`)
+						.addFields(
+							{ name: 'Founded', value: message.guild.createdAt },
+							{ name: 'Region', value: message.guild.region },
+							{ name: 'Owner', value: message.guild.owner },
+							{ name: 'Members', value: message.guild.memberCount },
+							{ name: 'Rules', value: message.guild.rulesChannel }
+						)
+						.setTimestamp()
+					);
+					break;
+				case 'user':
+					return message.reply(`your username is ${message.author.username} and your user ID is ${message.author.id}`);
+				default:
+					message.channel.send("On which subject doth thee seek knowledge?");
+			}
 	}
 });
 
 // Automatically reconnect if the bot disconnects due to inactivity
 bot.on('disconnect', function(erMsg, code) {
-	console.log('|----- Bot disconnected from Discord with code', code, 'for reason:', erMsg, '-----|');
+	console.log('\x1b[31m%s\x1b[0m', `--- Bot disconnected from Discord with code ${code} for the following reason: ${erMsg} ---`);
 	bot.connect();
 });
